12daa2bf828fc5db5773f4a911e09d59717fe9f3
ahmedelsideeg <elsideeg.a@mozn.sa>
2025-10-08 12:14:39 +0300

    Refactor component prop types from interface to type for consistency in Input, Label, and Textarea components; remove unused Button import from Navbar.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,6 @@
 import * as React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { LanguageToggle } from "@/components/LanguageToggle";
 
